55e51e68bcdb8b74b83142f0d0ccc5132d3fc1c2
Chonrattiz <66011212083@msu.ac.th>
2025-10-17 00:09:07 +0700

    dssd

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -4,8 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class Constants {
-  // public readonly API_ENDPOINT: string =
-  //   'https://api-project-adv-web.onrender.com';
+  public readonly API_ENDPOINT: string = 'https://game-point-api.onrender.com';
 
-  public readonly API_ENDPOINT: string = 'http://localhost:8080';
+  // public readonly API_ENDPOINT: string = 'http://localhost:8080';
 }
